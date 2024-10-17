9236cc4109c4edc62ea6fae70d2cfa712b82d4c6
hjhwang <hjhwang@jobis.co>
2024-10-17 23:29:06 +0900

    test

--- execute_worker_pipeline.sh
+++ execute_worker_pipeline.sh
@@ -2,23 +2,23 @@
 
 workers_status=$1
 CODEBUILD_RESOLVED_SOURCE_VERSION=$2
+ENV=$3
 
 function execute_worker_pipeline {
-  status=($(echo $workers_status | tr ":#" "\n"))
-  echo $CODEBUILD_RESOLVED_SOURCE_VERSION
+  status=($(echo ${workers_status} | tr ":#" "\n"))
   execute_all_pipelines="true"
   for (( i=0; i<${#status[@]}; i+=2 )); do
     if [[ ${status[i+1]} == "true" ]]; then
       execute_all_pipelines="false"
-      pipeline_name=data-ai-apne2-ultron-${status[i]//_/-}-pipeline-stg
-      echo $pipeline_name
+      pipeline_name=data-ai-apne2-ultron-${status[i]//_/-}-pipeline-${ENV}
+      echo ${pipeline_name}
       #aws codepipeline start-pipeline-execution --name ${pipeline_name} --source-revisions actionName=Source,revisionType=COMMIT_ID,revisionValue=${CODEBUILD_RESOLVED_SOURCE_VERSION}
     fi
   done
   if [[ $execute_all_pipelines == "true" ]]; then
     for (( i=0; i<${#status[@]}; i+=2 )); do
-      pipeline_name=data-ai-apne2-ultron-${status[i]//_/-}-pipeline-stg
-      echo "all executing: "$pipeline_name
+      pipeline_name=data-ai-apne2-ultron-${status[i]//_/-}-pipeline-${ENV}
+      echo "!@#$ "${pipeline_name}
       #aws codepipeline start-pipeline-execution --name ${pipeline_name} --source-revisions actionName=Source,revisionType=COMMIT_ID,revisionValue=${CODEBUILD_RESOLVED_SOURCE_VERSION}
     done
   fi
--- update_worker_status.sh
+++ update_worker_status.sh
@@ -5,16 +5,16 @@ file_path=$2
 
 function update_workers_status {
   updated_status=""
-  status=($(echo $workers_status | tr ":#" "\n"))
+  status=($(echo ${workers_status} | tr ":#" "\n"))
   for (( i=0; i<${#status[@]}; i+=2 )); do
-    if [[ $file_path =~ "/"${status[i]}"/"  ]]; then
-      updated_status=$updated_status"#"${status[i]}":true"
+    if [[ ${file_path} =~ "/"${status[i]}"/"  ]]; then
+      updated_status=${updated_status}"#"${status[i]}":true"
     else
-      updated_status=$updated_status"#"${status[i]}":"${status[i+1]}
+      updated_status=${updated_status}"#"${status[i]}":"${status[i+1]}
     fi
   done
   echo ${updated_status:1}
 }
 
 updated_status=$(update_workers_status)
-echo $updated_status
+echo ${updated_status}
