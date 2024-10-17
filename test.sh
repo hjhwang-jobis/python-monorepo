3c879bd9f216f9bf97f57bd882e8cb8376640021
hjhwang <hjhwang@jobis.co>
2024-10-17 16:26:11 +0900

    test

--- test.sh
+++ test.sh
@@ -6,6 +6,7 @@ var=$2
 function process_map {
   target=($(echo $1 | tr ":#" "\n"))
   for (( i=0; i<${#targets[@]}; i+=2 )); do
+    echo "mapdata " ${targets[i]}" var " $2
     if [[ ${targets[i]} =~ $2 ]]; then
       echo ${targets[i+1]} | tr -d " "
       exit
@@ -13,6 +14,5 @@ function process_map {
   done
 }
 
-echo "mapdata " $map_data " var " $var
 data=$(process_map $map_data $var)
 echo "the value is " $data
