93eafc4a68af847c12c1211ae86cc6b16ed43456
hjhwang <hjhwang@jobis.co>
2024-10-17 22:40:33 +0900

    test

--- execute_worker_pipeline.sh
+++ execute_worker_pipeline.sh
@@ -5,7 +5,7 @@ workers_status=$1
 function execute_worker_pipeline {
   status=($(echo $1 | tr ":#" "\n"))
   execute_all_pipelines="true"
-  for ( i=0; i<${#status[@]}; i+=2 ); do
+  for (( i=0; i<${#status[@]}; i+=2 )); do
     if [[ ${status[i+1]} = "true" ]]; then
       pipeline_name=data-ai-apne2-ultron-${status[i]//_/-}-pipeline-stg
       echo "not executing all pipelines: "${pipeline_name}
@@ -14,11 +14,11 @@ function execute_worker_pipeline {
     aws --version
   done
   if [[ execute_all_pipelines = "true" ]]; then
-    for ( i=0; i<${#status[@]}; i+=2 ); do
+    for (( i=0; i<${#status[@]}; i+=2 )); do
       pipeline_name=data-ai-apne2-ultron-${status[i]//_/-}-pipeline-stg
       echo "executing all pipelines: "${pipeline_name}
     done
   fi
 }
 
-get_worker_status $workers_status
+execute_worker_pipeline $workers_status
