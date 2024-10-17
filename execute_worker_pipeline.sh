9987b0a09e691bb1e778c01e6cced4d3c6eb0325
hjhwang <hjhwang@jobis.co>
2024-10-17 22:53:22 +0900

    i

--- execute_worker_pipeline.sh
+++ execute_worker_pipeline.sh
@@ -6,14 +6,14 @@ function execute_worker_pipeline {
   status=($(echo $1 | tr ":#" "\n"))
   execute_all_pipelines="true"
   for (( i=0; i<${#status[@]}; i+=2 )); do
-    if [[ ${status[i+1]} = "true" ]]; then
+    if [[ ${status[i+1]} == "true" ]]; then
       execute_all_pipelines="false"
     fi
     pipeline_name=data-ai-apne2-ultron-${status[i]//_/-}-pipeline-stg
     echo "not executing all pipelines: "${pipeline_name}", status: "${status[i+1]}", package_name: "${status[i]}
   done
   echo "execute_all_pipeline value is: "$execute_all_pipelines
-  if [[ execute_all_pipelines = "true" ]]; then
+  if [[ execute_all_pipelines == "true" ]]; then
     for (( i=0; i<${#status[@]}; i+=2 )); do
       pipeline_name=data-ai-apne2-ultron-${status[i]//_/-}-pipeline-stg
       echo "executing all pipelines: "${pipeline_name}", status: "${status[i+1]}", package_name: "${status[i]}
