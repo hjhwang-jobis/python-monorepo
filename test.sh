1cfb27de3f9315ecc6a9fcd8087dc6012a1da5b2
hjhwang <hjhwang@jobis.co>
2024-10-17 16:54:41 +0900

    tst

--- test.sh
+++ test.sh
@@ -3,9 +3,21 @@
 map_status=$1
 worker_name=$2
 
-function check_updated_worker {
+
+function update_worker_status {
   status=($(echo $1 | tr ":#" "\n"))
   for (( i=0; i<${#status[@]}; i+=2 )); do
+    if [[ ${status[i]} =~ $2 ]]; then
+      echo ${status[i+1]} | tr -d " "
+      exit
+    fi
+  done
+  echo "false"
+}
+
+function check_updated_worker {
+  status=($(echo $1 | tr ":#" "\n"))
+  for ( i=0; i<${#status[@]}; i+=2 ); do
     if [[ ${status[i]} =~ $2 ]]; then
       echo ${status[i+1]} | tr -d " "
       exit
