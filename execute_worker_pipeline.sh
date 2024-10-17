b64f71235576a7948a3ab0d779079b8f4dde59f1
hjhwang <hjhwang@jobis.co>
2024-10-17 22:47:22 +0900

    test

--- execute_worker_pipeline.sh
+++ execute_worker_pipeline.sh
@@ -7,17 +7,16 @@ function execute_worker_pipeline {
   execute_all_pipelines="true"
   for (( i=0; i<${#status[@]}; i+=2 )); do
     if [[ ${status[i+1]} = "true" ]]; then
-      pipeline_name=data-ai-apne2-ultron-${status[i]//_/-}-pipeline-stg
-      echo "not executing all pipelines: "${pipeline_name}
       execute_all_pipelines="false"
     fi
-    aws --version
+    pipeline_name=data-ai-apne2-ultron-${status[i]//_/-}-pipeline-stg
+    echo "not executing all pipelines: "${pipeline_name}", status: "${status[i+1]}", package_name: "${status[i]}
   done
   echo "execute_all_pipeline value is: "$execute_all_pipelines
   if [[ execute_all_pipelines = "true" ]]; then
     for (( i=0; i<${#status[@]}; i+=2 )); do
       pipeline_name=data-ai-apne2-ultron-${status[i]//_/-}-pipeline-stg
-      echo "executing all pipelines: "${pipeline_name}
+      echo "executing all pipelines: "${pipeline_name}", status: "${status[i+1]}", package_name: "${status[i]}
     done
   fi
 }
